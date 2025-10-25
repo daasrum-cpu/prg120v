5bc56316e009ddf4cac595a792c8a83f5093919a
daasrum-cpu <daasrum@gmail.com>
2025-10-25 17:16:23 +0200

    Update vis_alle_klasser.php

--- vis_alle_klasser.php
+++ vis_alle_klasser.php
@@ -3,7 +3,7 @@
 
 include("db-tilkobling.php");
 
-$sqlSetning = "SELECT * FROM klasse ORDER BY klassen_id;";
+$sqlSetning = "SELECT * FROM klasse ORDER BY klasse_id;";
 $sqlResultat = mysqli_query($db, $sqlSetning) or die ("ikke muligt å henta data fra databasen");
 
 $antallRader = mysqli_num_rows($sqlResultat);
