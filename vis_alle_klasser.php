00cc46e83649c0ad99d02d2be79976e5f3e38552
daasrum-cpu <daasrum@gmail.com>
2025-10-25 17:11:02 +0200

    Update vis_alle_klasser.php

--- vis_alle_klasser.php
+++ vis_alle_klasser.php
@@ -1,2 +1,22 @@
 <?php
-print ("hello");
+
+
+include("db_tilkobling.php");
+
+$sqlSetning = "SELECT * FROM klassen ORDER BY klassen_id;";
+$sqlResultat = mysqli_query($db, $sqlSetning) or die ("ikke muligt å henta data fra databasen");
+
+$antallRader = mysqli_num_rows($sqlResultat);
+
+print("<h3>Registrerte Klasser</h>");
+print("<table border='1'>");
+print("<tr><th align=left>klassekode</th><th align=left>klassenavn</th><th align=left>studiumkode</th>");
+
+for ($r=1;$r<=$antallRader;$r++)
+{
+    $rad=mysqli_fetch_array($sqlResultat);
+    $klassenavn=$rad['klassenavn'];
+    $klassekode=$rad['klassekode'];
+    $studiumkode=$rad['studiumkode'];
+}
+print ("</tabel>");
