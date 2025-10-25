6fa93af16af37d4d1b3e44bef12a4a65c04f8e8c
daasrum-cpu <daasrum@gmail.com>
2025-10-25 19:32:31 +0200

    Update slett_klasse.php

--- slett_klasse.php
+++ slett_klasse.php
@@ -1,6 +1,11 @@
 <h3>Slett Klasse</h3>
 
-<form method="post" action="" id="slettklasse" name="slettklasse" onSubmit="return bekreft();">
+<form method="post" action="" id="slettklasse" name="slettklasse" onSubmit="function bekreft()">
+    <script>
+        function bekreft() {
+            return confirm("Er du dikker på at du vil slette denne klassen?");
+        }
+    </script>
     klasse
     <select name="klassekode" id="klassekode">
         <?php
@@ -31,7 +36,7 @@
          $resultat = mysqli_query($db, $sjekk);
 
          if (mysqli_num_rows($resultat) > 0) {
-            print("Kan ikek slette klasse - Det finnes studenter i denne klassen");
+            print("Kan ikke slette klasse - Det finnes studenter i denne klassen");
          } else {
             $sqlsetning = "DELETE FROM klasse WHERE klassekode = '$klassekode';";
             mysqli_query($db,$sqlsetning) or die ("Ikke muligt å slette data fra databasen");
