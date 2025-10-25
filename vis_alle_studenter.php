489c0a9bb9884c6d7a3be5ad0eb269496b73255c
daasrum-cpu <daasrum@gmail.com>
2025-10-25 18:51:28 +0200

    Update vis_alle_studenter.php

--- vis_alle_studenter.php
+++ vis_alle_studenter.php
@@ -10,17 +10,17 @@ $antallRader = mysqli_num_rows($sqlResultat);
 
 print("<h3>Registrerte Studenter</h>");
 print("<table border='1'>");
-print("<tr><th align=left>brukernavn</th><th align=left>Fornavn</th><th align=left>studiumkode</th><th align=left>klassekode</th>");
+print("<tr><th align=left>Brukernavn</th><th align=left>Fornavn</th><th align=left>Etternavn</th><th align=left>Klassekode</th>");
 
 for ($r=1;$r<=$antallRader;$r++)
 {
     $rad=mysqli_fetch_array($sqlResultat);
-    $Fornavn=$rad['Fornavn'];
+    $fornavn=$rad['fornavn'];
     $brukernavn=$rad['brukernavn'];
-    $etternavn=$rad['studiumkode'];
+    $etternavn=$rad['etternavn'];
     $klassekode=$rad['klassekode'];
 
-    print("<tr><td>$brukernavn</td><td>$Fornavn</td><td>$etternavn</td><td>$klassekode</td></tr>");
+    print("<tr><td>$brukernavn</td><td>$fornavn</td><td>$etternavn</td><td>$klassekode</td></tr>");
 }
 print ("</tabel>");
 
