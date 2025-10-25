405956664ad50b0050932ba8a141c4381eb18047
daasrum-cpu <daasrum@gmail.com>
2025-10-25 18:25:48 +0200

    Update registrer_student.php

--- registrer_student.php
+++ registrer_student.php
@@ -1,23 +1,27 @@
 <h2>Registrer student</h2>
 
 <form method="post" action="" id="registrer_student_skjema" name="registrer_student_skjema">
-    studentkode <input type="text" id="studentkode" name="studentkode" required /> <br/>
-    studentnavn <input type="text" id="studentnavn" name="studentnavn" required /> <br/>
+    brukernavn <input type="text" id="brukernavn" name="brukernavn" required /> <br/>
+    fornavn <input type="text" id="fornavn" name="fornavn" required /> <br/>
+    etternavn <input type="text" id="etternavn" name="etternavn" required /> <br/>
+    klassekode <input type="text" id="klassekode" name="klassekode" required /> <br/>
     <input type="submit" value="registrer student" id="registrerstudentknapp" name="registrerstudentknapp" />
     <input type="reset" value="nulstill" id="nullstill" name="nulstill" /> <br/>
 </form>
 
 <?php
 if (isset($_POST['registrerstudentknapp'])) {
-    $studentkode = $_POST['studentkode'];
-    $studentnavn = $_POST['studentnavn'];
+    $brukernavn = $_POST['brukernavn'];
+    $fornavn = $_POST['fornavn'];
+    $etternavn = $_POST['etternavn'];
+    $klassekode = $_POST['klassekode'];
 
-    if (!$studentnavn || !$studentkode) {
+    if (!$fornavn || !$brukernavn || !$etternavn || !$klassekode) {
         print ("alle felt må fylles ut");
     }
     else {
         include("db-tilkobling.php");
-        $sqlSetning = "SELECT * FROM student WHERE studentkode = '$studentkode';";
+        $sqlSetning = "SELECT * FROM student WHERE brukernavn = '$brukernavn';";
         $sqlResultat=mysqli_query($db,$sqlSetning) or die ("ikke mulig å hente data fra databasen");
         $antallRader=mysqli_num_rows($sqlResultat);
 
@@ -25,11 +29,11 @@ if (isset($_POST['registrerstudentknapp'])) {
             print ("studentn finnes fra før");
         }
         else {
-            $sqlSetning = "INSERT INTO student (studentkode, studentnavn)
-            VALUES('$studentkode','$studentnavn','$');";
+            $sqlSetning = "INSERT INTO student (brukernavn, fornavn, etternavn, klassekode)
+            VALUES('$brukernavn','$fornavn','$etternavn','$klassekode');";
             mysqli_query($db,$sqlSetning) or die ("ikke muligt å registrere i databasen");
 
-            print ("følgende student er nå lagt til $studentkode $studentnavn");
+            print ("følgende student er nå lagt til $fornavn $etternavn");
         }
     }
 }
