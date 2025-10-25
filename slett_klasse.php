805730dca96ceae3a5319d74fdc2958afc366585
daasrum-cpu <daasrum@gmail.com>
2025-10-25 17:40:38 +0200

    Update slett_klasse.php

--- slett_klasse.php
+++ slett_klasse.php
@@ -1,1 +1,30 @@
+
+<h3>Slett Klasse</h3>
+
+<form mothod="post" action="" id="slettklasse" name="slettklasse" onSubmit="return bekreft()">
+    klasse
+    <select name="klassekode" id="klassekode">
+        <?php print ("<option value=">velg klasse </option>);
+
+    </select> <br/>
+    <input type="submit" value="Slett klasse" name="slettklasse" id="slettklasse" />
+</form>
+
 <?php
+ if (isset($_POST['slettklasse'])) {
+     $klassenavn=$_POST['klassenavn'];
+
+     if (!$klassekode) {
+         print ("det er ikke valgt en klasse");
+
+     }
+     else
+     {
+         include("db-tilkobling.php");
+         $sqlsetning = "DELETE FROM klasse WHERE klassenavn = '$klassenavn';";
+         mysqli_query($db,$sqlsetning) or die ("ikke muligt å slette data fra databasen");
+
+         print("følgende klasse er slettet: $klassenavn");
+     }
+ }
+ 
